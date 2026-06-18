1cfd71d6ccdb740208118204cd80370887133bf1
Mukesh <mukesh@Mukeshs-MacBook-Pro.local>
2026-06-18 21:11:10 +0530

    redis and logger impl

--- repository/document_repository.py
+++ repository/document_repository.py
@@ -1,18 +1,25 @@
-from sqlalchemy import text
+from sqlalchemy import select, text
 from sqlalchemy.orm import Session
+from models import Document
 from query import DENSE_SEARCH_QUERY, SPARSE_SEARCH_QUERY
+from clients import encode_one
+from clients.redis_client import redis
 
 
 class DocumentRepository():
 
     def __init__(self, db: Session) -> None:
         self.db = db
-
     
-    async def get_dense_search_results(self):
-        return self.db.execute(text(DENSE_SEARCH_QUERY))
+    async def get_dense_search_results(self, query: str, top_k):
+        query_embedding = await encode_one(query)
+        return self.db.execute(text(DENSE_SEARCH_QUERY), {"query_embedding": query_embedding, "top_k": top_k}).fetchall()
 
-    async def get_sparse_search_results(self):
-        return self.db.execute(text(SPARSE_SEARCH_QUERY))
+    async def get_sparse_search_results(self, query: str, top_k):
+        return self.db.execute(text(SPARSE_SEARCH_QUERY), {"query": query, "top_k": top_k}).fetchall()
+
+    async def get_all_documents(self):
+        result = self.db.execute(select(Document.id, Document.content))
+        return result.mappings().all()
 
         
--- seed.py
+++ seed.py
@@ -5,30 +5,30 @@ from models import Document
 from services import process_data, already_seeded
 
 
-def seed(db: Session):
-    if already_seeded(db):
-        print("Documents already seeded to db")
-        return
+# def seed(db: Session):
+#     if already_seeded(db):
+#         print("Documents already seeded to db")
+#         return
     
-    print("Seeding started")
-    unique_contexts = process_data()
-    total_context_count = len(unique_contexts)
-    print(f"Total context count : {total_context_count}")
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        futures_to_be_completed = {
-            executor.submit(encode_one, context): (idx, context)
-            for idx, context in enumerate(unique_contexts)
-        }
-        for future in as_completed(futures_to_be_completed):
-            idx, context = futures_to_be_completed[future]
-            embedding = future.result()
-            new_document = Document(content=context, embedding=embedding)
-            db.add(new_document)
-            if idx % 100 == 0:
-                print(f"{idx} Records processed ") 
-    db.commit()
+#     print("Seeding started")
+#     unique_contexts = process_data()
+#     total_context_count = len(unique_contexts)
+#     print(f"Total context count : {total_context_count}")
+#     with ThreadPoolExecutor(max_workers=20) as executor:
+#         futures_to_be_completed = {
+#             executor.submit(encode_one, context): (idx, context)
+#             for idx, context in enumerate(unique_contexts)
+#         }
+#         for future in as_completed(futures_to_be_completed):
+#             idx, context = futures_to_be_completed[future]
+#             embedding = future.result()
+#             new_document = Document(content=context, embedding=embedding)
+#             db.add(new_document)
+#             if idx % 100 == 0:
+#                 print(f"{idx} Records processed ") 
+#     db.commit()
 
-    print(f"Seeded {total_context_count} documents")
+#     print(f"Seeded {total_context_count} documents")
 
 
     
